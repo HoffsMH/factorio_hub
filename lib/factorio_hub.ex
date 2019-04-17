c1bd82aadaa2ac959234bbce6bd2707d803af94a
HoffsMH <hecker.matthew@gmail.com>
2019-04-17 14:43:45 +0900

    initial commit

---
+++ lib/factorio_hub.ex
@@ -0,0 +1,18 @@
+defmodule FactorioHub do
+  @moduledoc """
+  Documentation for FactorioHub.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> FactorioHub.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end

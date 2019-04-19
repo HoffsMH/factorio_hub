1c02a7e9ae507e9846cc358d7516fba0d5247200
HoffsMH <hecker.matthew@gmail.com>
2019-04-19 13:45:25 +0900

    adds faker dependency to test and dev only

--- mix.exs
+++ mix.exs
@@ -24,7 +24,8 @@ defmodule FactorioHub.MixProject do
     [
       {:ecto, ">= 0.0.0"},
       {:postgrex, ">= 0.0.0"},
-      {:ecto_sql, ">= 0.0.0"}
+      {:ecto_sql, ">= 0.0.0"},
+      {:faker, ">= 0.0.0", only: [:test, :dev]}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]

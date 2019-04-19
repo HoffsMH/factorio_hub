86af154dfd914c548769a8b43a4694f3c52291e4
HoffsMH <hecker.matthew@gmail.com>
2019-04-19 19:20:32 +0900

    cleaning up mix file

--- mix.exs
+++ mix.exs
@@ -1,34 +1,35 @@
 defmodule FactorioHub.MixProject do
   use Mix.Project
+  @name :factorio_hub
+  @version "0.1.0"
+  @deps [
+    {:ecto, ">= 0.0.0"},
+    {:postgrex, ">= 0.0.0"},
+    {:ecto_sql, ">= 0.0.0"},
+    {:faker, ">= 0.0.0", only: [:test, :dev]},
+    {:absinthe, ">= 0.0.0"}
+  ]
+
+  @aliases [
+    "ecto.reset": ["ecto.drop --quiet", "ecto.create --quiet", "ecto.migrate"],
+    "seeds": ["run ./priv/repo/seeds/main.exs"]
+  ]
 
   def project do
     [
-      app: :factorio_hub,
-      version: "0.1.0",
+      app: @name,
+      version: @version,
       elixir: "~> 1.8",
+      aliases: @aliases,
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: @deps
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       mod: {FactorioHub.Application, []},
       extra_applications: [:logger]
-    ]
-  end
-
-  # Run "mix help deps" to learn about dependencies.
-  defp deps do
-    [
-      {:ecto, ">= 0.0.0"},
-      {:postgrex, ">= 0.0.0"},
-      {:ecto_sql, ">= 0.0.0"},
-      {:faker, ">= 0.0.0", only: [:test, :dev]},
-      {:absinthe, ">= 0.0.0"}
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
   end
 end

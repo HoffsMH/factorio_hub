4690154b0605cc125f8a36bc1a8bc0487bb43fb5
HoffsMH <hecker.matthew@gmail.com>
2019-04-19 13:31:59 +0900

    this doesnt belong on the top level

--- lib/factorio_hub/blueprint_repository.ex
+++
@@ -1,9 +0,0 @@
-defmodule FactorioHub.UserEntry.BlueprintRepository do
-  use Ecto.Schema
-  import Ecto.Changeset
-  @primary_key {:id, :binary_id, autogenerate: true}
-
-  schema "blueprint_repository" do
-    timestamps()
-  end
-end

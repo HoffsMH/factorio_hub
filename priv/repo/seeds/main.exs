c0da117fe7a6e4a0913bba8b8edd69ab056aeea3
HoffsMH <hecker.matthew@gmail.com>
2019-04-19 15:31:54 +0900

    blueprint repos and users are now connected

---
+++ priv/repo/seeds/main.exs
@@ -0,0 +1,21 @@
+# Create alot of users and attach blueprint repos to a portion of them
+
+alias FactorioHub.Repo
+alias FactorioHub.Repo.{User, BlueprintRepository}
+alias Faker.{Name}
+for _ <- 1..100 do
+    Enum.map(1..300, fn _->
+      Task.async(fn ->
+        {:ok, user } = Repo.insert(%User{
+            name: "#{Name.last_name}#{:rand.uniform(1000)}"
+                                   })
+
+        if :rand.uniform(3) > 2 do
+          for _ <- 0..:rand.uniform(2) do
+            Repo.insert(%BlueprintRepository{ owner: user })
+          end
+        end
+      end)
+    end)
+    |> Enum.each(&Task.await(&1))
+end
